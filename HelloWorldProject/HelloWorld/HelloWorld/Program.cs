e310daa6f2459e111546c689fc2200a69cfdcc49
Demo Git Account <rahmat15-6970@diu.edu.bd>
2021-08-31 15:50:21 +0600

    variables

--- HelloWorldProject/HelloWorld/HelloWorld/Program.cs
+++ HelloWorldProject/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //Console.WriteLine("Hello World");
+            /*byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f; //for float f, decemal m
+            char ch = 'A';
+            string firstName = "Maruf";
+            bool inWorking = false;*/
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f; //for float f, decemal m
+            var ch = 'A';
+            var firstName = "Maruf";
+            var inWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(ch);
+            Console.WriteLine(firstName);
+            Console.WriteLine(inWorking);
         }
     }
 }
