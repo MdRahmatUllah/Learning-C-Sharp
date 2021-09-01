526cdc6181fc63f679fb8382b96586a211a7ab20
Demo Git Account <rahmat15-6970@diu.edu.bd>
2021-09-01 19:57:22 +0600

    creating separate files for each class

---
+++ HelloWorldProject/HelloWorld/HelloWorld/inputClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class inputClass
+    {
+        public int inputInt()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ HelloWorldProject/HelloWorld/HelloWorld/outputClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class outputClass
+    {
+        public int num1;
+        public int num2;
+        public char sign;
+        public int res;
+
+        public void setValue()
+        {
+            var clc = new Calculator();
+            num1 = clc.number1;
+            num2 = clc.number2;
+            sign = clc.sign;
+            res = clc.res;
+        }
+
+        public void printOutputString()
+        {
+            setValue();
+            Console.WriteLine(num1 + " " + sign + " " + num2 + " = " + res);
+        }
+
+        public void printString(string st)
+        {
+            Console.WriteLine(st);
+        }
+    }
+}
