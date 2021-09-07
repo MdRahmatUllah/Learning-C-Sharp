44e45119868f3c75572bcbcb1238e1c03648056d
Demo Git Account <rahmat15-6970@diu.edu.bd>
2021-09-07 11:57:54 +0600

    Speed Camera and Orientation

---
+++ HelloWorldProject/HelloWorld/HelloWorld/Orientation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Orientation
+    {
+        public void Main()
+        {
+            var inp = new inputClass();
+
+            int width = inp.inputInt();
+            int height = inp.inputInt();
+
+            if(width > height)
+                Console.WriteLine("Landscape");
+            else if(height > width)
+                Console.WriteLine("Portrait");
+            else
+                Console.WriteLine("Invalid size or Squre");
+        }
+    }
+}
---
+++ HelloWorldProject/HelloWorld/HelloWorld/SpeedCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class SpeedCamera
+    {
+
+        
+
+        public void Main()
+        {
+
+            var inp = new inputClass();
+
+            while (true)
+            {
+                Console.WriteLine("What is the speed limit? ");
+                var lim = inp.inputInt();
+                var speed = inp.inputInt();
+
+                if (lim >= speed)
+                    Console.WriteLine("Ok");
+                else
+                {
+                    var dem = (speed - lim) / 5;
+
+                    if (dem > 12)
+                        Console.WriteLine("Suspended");
+                    else
+                        Console.WriteLine("Speed limit crossed {0}", dem);
+                }
+            }
+        }
+    }
+}
