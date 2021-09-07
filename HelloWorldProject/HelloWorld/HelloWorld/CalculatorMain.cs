1752ab32734ee0d0bbcd6aa5e781211a8335dcf4
Demo Git Account <rahmat15-6970@diu.edu.bd>
2021-09-07 10:25:03 +0600

    added

---
+++ HelloWorldProject/HelloWorld/HelloWorld/CalculatorMain.cs
@@ -0,0 +1,53 @@
+﻿namespace HelloWorld
+{
+    public class CalculatorMain
+    {
+        public void Clc()
+        {
+            var inp = new inputClass();
+            var outp = new outputClass();
+
+            outp.printString("Enter the first Number: ");
+
+            int number1;
+            number1 = inp.inputInt();
+            outp.printString("Enter the second Number: ");
+
+            int number2;
+            number2 = inp.inputInt();
+
+            int fl = 0;
+
+            outp.printString("For Add press 1\nFor Substractions 2\nFor Division 3\n" +
+                "For Multiplications 4");
+
+            fl = inp.inputInt();
+
+            var myCalc = new Calculator();
+
+            myCalc.number1 = number1;
+            myCalc.number2 = number2;
+
+            switch (fl)
+            {
+                case 1:
+                    myCalc.Add();
+                    break;
+                case 2:
+                    myCalc.Substraction();
+                    break;
+                case 3:
+                    myCalc.Multiplications();
+                    break;
+                case 4:
+                    myCalc.Division();
+                    break;
+                default:
+                    outp.printString("Invalid Input");
+                    break;
+            }
+            outp.printOutputString();
+        }
+
+    }
+}
