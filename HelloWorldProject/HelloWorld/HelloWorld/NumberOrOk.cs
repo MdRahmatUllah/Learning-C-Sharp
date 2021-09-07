e765b77a457314af949601608021540e62b9a917
Demo Git Account <rahmat15-6970@diu.edu.bd>
2021-09-07 13:57:08 +0600

    Enter a number or ok to exit print sum

---
+++ HelloWorldProject/HelloWorld/HelloWorld/NumberOrOk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CSharpLearning
+{
+    class NumberOrOk
+    {
+        public void Main()
+        {
+            var sum = 0;
+            while(true)
+            {
+                var inp = Console.ReadLine();
+
+                if ("ok" == inp) break;
+
+                sum += Convert.ToInt32(inp);
+            }
+
+            Console.WriteLine("The Sum is {0}", sum);
+        }
+    }
+}
